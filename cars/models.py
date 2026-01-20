1f904328ac69810f8dc3029125475ab22cd6162e
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2026-01-19 22:42:44 -0300

    adicionando os models no projeto

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    id = models.Autofield(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
