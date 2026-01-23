ec02e7e5d8e2754d683054b66f26985fb2a3afd5
joao <dev@monitoo.com.br>
2026-01-23 11:42:01 -0300

    feat: criando o template de carros e ajustando a view.

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def cars_view(request):
-    return HttpResponse('Meus carros 1')
+    return render(request, 'cars.html')
 
