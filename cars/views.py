a8837a33eb87600219c93cde308e3cdd324d0d9d
joao <dev@monitoo.com.br>
2026-01-26 10:23:08 -0300

    buscando carros no banco de dados com django ORM

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from cars.models import Car
 
 
 def cars_view(request):
+    # consulta todos os campos e colunas da tabela Car
+    cars = Car.objects.all()
+
     return render(
         request, 
         'cars.html', 
-        {'cars': {'model': 'Golf GTI'} }
+        {'cars': cars }
     )
 
