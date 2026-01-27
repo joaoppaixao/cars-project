df80aa41b6026a9caa768d6e43e602a2f1919b6a
joao <dev@monitoo.com.br>
2026-01-27 10:09:41 -0300

    filtrando carros com os parametros do usuario

--- cars/views.py
+++ cars/views.py
@@ -3,8 +3,12 @@ from cars.models import Car
 
 
 def cars_view(request):
-    # consulta todos os campos e colunas da tabela Car
     cars = Car.objects.all()
+    search  = request.GET.get('search')
+
+    if search:
+    # filtra de acordo com a pesquisa do usuário com a variável search
+        cars = cars.filter(model__contains=search)
 
     return render(
         request, 
