fdaf9c23868ea795e541411a61e95b0ba386ede3
joao <dev@monitoo.com.br>
2026-02-05 16:48:19 -0300

    alterando as views para class based views

--- cars/views.py
+++ cars/views.py
@@ -1,34 +1,44 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('brand')
-    search  = request.GET.get('search')
+class CarsView(View):
+    
+    def get(self, request):
+        cars = Car.objects.all().order_by('brand')
+        search  = request.GET.get('search')
 
-    if search:
-    # filtra de acordo com a pesquisa do usuário com a variável search
-        cars = cars.filter(model__icontains=search)
+        if search:
+        # filtra de acordo com a pesquisa do usuário com a variável search
+            cars = cars.filter(model__icontains=search)
 
-    return render(
-        request, 
-        'cars.html', 
-        {'cars': cars }
+        return render(
+            request, 
+            'cars.html', 
+            {'cars': cars }
+        )
+    
+
+class NewCarView(View):
+
+    def get(self, request):
+        new_car_form = CarModelForm()
+
+        return render(
+                request, 
+                'new_car.html',
+                { 'new_car_form': new_car_form }
     )
 
-def new_car_view(request):
-    if request.method == 'POST':
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-        else:
-            print('[Erro]: não foi possível cadastrar o seu carro. ')
-    else:
-        new_car_form = CarModelForm()
-
-    return render(request, 
+        return render(
+                request, 
                 'new_car.html',
                 { 'new_car_form': new_car_form }
     )
